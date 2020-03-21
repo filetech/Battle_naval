b8c107f0e5e77b744caba098a0f93e5b3668c054
filetech <62456337+filetech@users.noreply.github.com>
2020-03-21 10:09:36 -0500

    Update BatallaNaval.java

--- BatallaNaval.java
+++ BatallaNaval.java
@@ -11,7 +11,7 @@ Lancha de Guerra (2 agujeros)
 Autor: Johan Astudillo
 
 Por: www.FileTechn.com
-+/
+*/
 
 import java.util.Random;
 import java.util.Scanner;
@@ -20,24 +20,24 @@ public class BatallaNaval {
     public static void main(String[] args) {
         int tamX, tamY;
         
-        //Lectura por consola del tamaño del tablero
+        //Lectura por consola del tamaÃ±o del tablero
         Scanner consola = new Scanner(System.in);
         do{
-            System.out.print("Total filas (mínimo 6): "); 
+            System.out.print("Total filas (mÃ­nimo 6): "); 
             tamX = consola.nextInt();
-            System.out.print("Total columnas (mínimo 6): "); 
+            System.out.print("Total columnas (mÃ­nimo 6): "); 
             tamY = consola.nextInt();
         }while(tamX<=5 || tamY<=5);
         
-        Random azar = new Random(); //El generador de números aleatorios
+        Random azar = new Random(); //El generador de nÃºmeros aleatorios
         
-        //Arreglo bdimensional que será el tablero de batalla naval. Se inicializa con el caracter punto
+        //Arreglo bdimensional que serÃ¡ el tablero de batalla naval. Se inicializa con el caracter punto
         char Tablero[][] = new char[tamX][tamY];
         for (int fila=0; fila < Tablero.length; fila++)
             for (int col=0; col < Tablero[fila].length; col++)
                 Tablero[fila][col] = '.';
         
-        //Ubica cada barco. Parámetros: generador de números aleatorios, tablero, número de agujeros, letra del barco
+        //Ubica cada barco. ParÃ¡metros: generador de nÃºmeros aleatorios, tablero, nÃºmero de agujeros, letra del barco
         CuadraBarco(azar, Tablero, 5, 'P');
         CuadraBarco(azar, Tablero, 4, 'D');
         CuadraBarco(azar, Tablero, 3, 'S');
@@ -55,29 +55,29 @@ public class BatallaNaval {
     //Cuadra el barco en el tablero evitando que se cruce con otro
     public static void CuadraBarco(Random azar, char[][] Tablero, int huecos, char barco){
         boolean posicionado; //Controla si el barco fue ubicado correctamente
-        int posX, posY; //Coordenadas de ubicación del barco
+        int posX, posY; //Coordenadas de ubicaciÃ³n del barco
         do{
-            posicionado = true; //Por defecto está bien ubicado
+            posicionado = true; //Por defecto estÃ¡ bien ubicado
             
-            if (azar.nextDouble()<0.5){ //Se ubicará horizontalmente el barco
+            if (azar.nextDouble()<0.5){ //Se ubicarÃ¡ horizontalmente el barco
                 posX = azar.nextInt(Tablero.length-huecos);
                 posY = azar.nextInt(Tablero[posX].length);
                 posicionado = true; 
                 
-                //Revisa si en esa ubicación solo hay '.', significa que no hay barco allí
+                //Revisa si en esa ubicaciÃ³n solo hay '.', significa que no hay barco allÃ­
                 for (int revisa=0; revisa<huecos; revisa++) if (Tablero[posX+revisa][posY] != '.') posicionado = false;
                 
-                //Si pasa la revisión, ubica el barco
+                //Si pasa la revisiÃ³n, ubica el barco
                 if (posicionado) for (int revisa=0; revisa<huecos; revisa++) Tablero[posX+revisa][posY]=barco;
             }
-            else{ //Se ubicará verticalmente el barco
+            else{ //Se ubicarÃ¡ verticalmente el barco
                 posX = azar.nextInt(Tablero.length);
                 posY = azar.nextInt(Tablero[posX].length-huecos);
                 
-                //Revisa si en esa ubicación solo hay '.', significa que no hay barco allí
+                //Revisa si en esa ubicaciÃ³n solo hay '.', significa que no hay barco allÃ­
                 for (int revisa=0; revisa<huecos; revisa++) if (Tablero[posX][posY+revisa] != '.') posicionado = false;
                 
-                //Si pasa la revisión, ubica el barco
+                //Si pasa la revisiÃ³n, ubica el barco
                 if (posicionado) for (int revisa=0; revisa<huecos; revisa++) Tablero[posX][posY+revisa]=barco;                
             }
         }while(!posicionado); //No sale del ciclo hasta que ubique el barco
